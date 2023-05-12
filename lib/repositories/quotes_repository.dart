2e0e26ab4eab28fb726fbe19f96a158c37908ebd
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2023-05-12 08:10:02 +0700

    🎈 perf: integrate API get randam quotes

---
+++ lib/repositories/quotes_repository.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:quotes_app/models/quotable_model.dart';
+import 'package:quotes_app/utils/api_path.dart';
+
+abstract class QuotesRepository {
+  Future<List<Quotable>> getQuotes();
+}
+
+class QuotesRepositoryImpl implements QuotesRepository {
+  @override
+  Future<List<Quotable>> getQuotes() async {
+    final response =
+        await http.get(Uri.parse('$baseUrl$quotesPath$randomPath?limit=20'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      final quotes = (data as List).map((e) => Quotable.fromJson(e)).toList();
+      return quotes;
+    } else {
+      throw Exception('Failed to load quotes');
+    }
+  }
+}
+
+final quotesRepositoryProvider =
+    Provider<QuotesRepository>((ref) => QuotesRepositoryImpl());
--- lib/views/pages/quotes_page.dart
+++ lib/views/pages/quotes_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:quotes_app/controllers/quotes_controller.dart';
 import 'package:quotes_app/views/pages/quote_detail_page.dart';
+import 'package:quotes_app/views/themes/colors.dart';
 import 'package:quotes_app/views/themes/typography.dart';
 import 'package:quotes_app/views/widgets/icon_solid_light.dart';
 import 'package:quotes_app/views/widgets/quotes_card.dart';
@@ -11,6 +13,7 @@ class QuotesPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final quotesState = ref.watch(quotesProvider);
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -44,40 +47,62 @@ class QuotesPage extends ConsumerWidget {
             ),
           ),
           SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return Padding(
-                  padding: EdgeInsets.only(
-                    right: 16,
-                    left: 16,
-                    bottom: 16,
-                    top: index == 0 ? 100 : 0,
-                  ),
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const QuoteDetailPage(
-                            content:
-                                "The best way to get started is to quit talking and begin doing. The best way to get started is to quit talking and begin doing. The best way to get started is to quit talking and begin doing.",
-                            author: "Walt Disney",
-                            authorAvatar: "assets/img_avatar.png",
-                            authorJob: "Co-Founder of The Walt Disney Company",
-                          ),
+            delegate: quotesState.when(
+              data: (quotes) {
+                return SliverChildBuilderDelegate(
+                  (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(
+                        right: 16,
+                        left: 16,
+                        bottom: 16,
+                        top: index == 0 ? 100 : 0,
+                      ),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => QuoteDetailPage(
+                                content: quotes[index].content!,
+                                // "The best way to get started is to quit talking and begin doing. The best way to get started is to quit talking and begin doing. The best way to get started is to quit talking and begin doing.",
+                                author: "Walt Disney",
+                                authorAvatar: "assets/img_avatar.png",
+                                authorJob:
+                                    "Co-Founder of The Walt Disney Company",
+                              ),
+                            ),
+                          );
+                        },
+                        child: QuotesCard(
+                          author: quotes[index].author!,
+                          authorAvatar: "assets/img_avatar.png",
+                          authorJob: "Co-Founder of The Walt Disney Company",
+                          content: quotes[index].content!,
+                          // "The best way to get started is to quit talking and begin doing.",
                         ),
-                      );
-                    },
-                    child: const QuotesCard(
-                      author: "Walt Disney",
-                      authorAvatar: "assets/img_avatar.png",
-                      authorJob: "Co-Founder of The Walt Disney Company",
-                      content:
-                          "The best way to get started is to quit talking and begin doing.",
-                    ),
-                  ),
+                      ),
+                    );
+                  },
+                  childCount: 7,
                 );
               },
-              childCount: 7,
+              loading: () => SliverChildListDelegate.fixed(
+                [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        color: MyColors.primaryDark,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              error: (error, stackTrace) => const SliverChildListDelegate.fixed(
+                [
+                  Text("Something went wrong!"),
+                ],
+              ),
             ),
           )
         ],
